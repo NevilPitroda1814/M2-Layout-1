27299a6364785f2785f1b80bafd0b7dced095163
NevilPitroda1814 <123956050+NevilPitroda1814@users.noreply.github.com>
2023-03-07 16:15:22 +0530

    Maratho-2 Layout-1
    create a js file for Scripting a layout-1

---
+++ script.js
@@ -0,0 +1,53 @@
+const container = document.querySelector('.container')
+const sizeEl = document.querySelector('.size')
+const color = document.querySelector('.color')
+const resetBtn = document.querySelector('.btn')
+
+let size = sizeEl.value
+let draw = false
+
+function populate(size) {
+  container.style.setProperty('--size', size)
+  for (let i = 0; i < size * size; i++) {
+    const div = document.createElement('div')
+    div.classList.add('pixel')
+
+    div.addEventListener('mouseover', function(){
+        if(!draw) return
+        div.style.backgroundColor = color.value
+    })
+    div.addEventListener('mouseover', function(){
+        div.style.backgroundColor = color.value
+    })
+
+    container.appendChild(div)
+  }
+}
+
+window.addEventListener("mousedown", function(){
+    draw = true
+})
+window.addEventListener("mouseup", function(){
+    draw = false
+})
+
+function reset(){
+    container.innerHTML = ''
+    populate(size)
+}
+// Reset a container...
+resetBtn.addEventListener('click', reset)
+
+sizeEl.addEventListener('keyup', function(){
+    size = sizeEl.value
+    reset()
+})
+// Erase a color ...
+function eraseColor(){
+    container.addEventListener('mouseover', e => {
+    e.target.style.backgroundColor = null;
+  });
+}
+
+
+populate(size)
